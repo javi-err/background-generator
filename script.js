80f68c7b041b7d83e7a05967993a4a84bf392f28
javi <jsmart1993@hotmail.com>
2020-09-29 23:48:40 -0700

    complete project

---
+++ script.js
@@ -0,0 +1,22 @@
+var css = document.querySelector('h2')
+var color1 = document.querySelector('.color1')
+var color2 = document.querySelector('.color2')
+var body = document.querySelector('body')
+
+function setColors() {
+    body.style.background = 
+    `linear-gradient(to right, 
+    ${color1.value} , ${color2.value})`;
+
+    css.textContent = body.style.background + ";";
+}
+
+
+color1.addEventListener("input", setColors);
+
+color2.addEventListener("input", setColors);
+
+setColors();
+
+
+console.log(body);  
